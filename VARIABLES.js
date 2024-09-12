a59bae1bfcb49bcd307403ad3121543459967fe7
juankinpro <juan.lozada696@pascualbravo.edu.co>
2024-09-11 19:11:07 -0500

    Básico hola mundo
    voy a ver si en const utilizo pi para medir la tierra,

--- VARIABLES.js
+++ VARIABLES.js
@@ -1,4 +1,5 @@
 //VARIABLES PARA EL 11/09/2024
+//LET
 let n1 = 50;
 let n2 = 100;
 let n4 = 0.5;
@@ -6,3 +7,7 @@ let Suma = n1 + n2;
 let Resta = n2 - n1;
 let Multi = n2 * n4;
 let Divi = n2 / n4;
+let Peticion = true;
+let HM = "HOLA MUNDO";
+
+
